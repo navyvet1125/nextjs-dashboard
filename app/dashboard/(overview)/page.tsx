92a751440e39db2a7c76179b47baaa925cef0af3
Evan Washington <enavy04@gmail.com>
2024-03-13 02:28:32 -0700

    Completed chapter 16 on metadata.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -12,6 +12,11 @@ const {
 
 import { Suspense } from 'react';
 import { LatestInvoicesSkeleton, RevenueChartSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default async function Page() {
   return (
